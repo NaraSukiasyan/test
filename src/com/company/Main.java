b983a30e2e1eed7a439413fb6eaeebe2c50601df
newuser <narasukiasyanb93@mail.ru>
2022-04-19 22:13:15 +0400

    test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,28 +1,33 @@
 package com.company;
-class Test{
+
+class Test {
     int a, b;
-    Test (int i,int j) {
-        a=i;
-        b=j;
+
+    Test(int i, int j) {
+        a = i;
+        b = j;
     }
-    boolean equalTo(Test o){
-      if  (o.a == a && o.b == b) return true;
-      else return false;
+
+    boolean equalTo(Test o) {
+        if (o.a == a && o.b == b) return true;
+        else return false;
     }
-    Test incrByTen(){
-        Test temp=new Test(a+10, b+10);
+
+    Test incrByTen() {
+        Test temp = new Test(a + 10, b + 10);
         return temp;
     }
 }
+
 public class Main {
 
 
     public static void main(String[] args) {
-	Test ob1=new Test (100,22);
-    Test ob2=new Test (100,22);
-    Test ob3=new Test (-1,-1);
-        System.out.println("ob1== ob2:"+ob1.equalTo(ob2));
-        System.out.println("ob1==ob3:"+ ob1.equalTo(ob3));
+        Test ob1 = new Test(100, 22);
+        Test ob2 = new Test(100, 22);
+        Test ob3 = new Test(-1, -1);
+        System.out.println("ob1== ob2:" + ob1.equalTo(ob2));
+        System.out.println("ob1==ob3:" + ob1.equalTo(ob3));
 
     }
 }
